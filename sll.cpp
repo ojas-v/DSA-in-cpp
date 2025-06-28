8982c3247c698f9938f68b22c94162ebb55a006d
ojas-v <ojasvaidya17@gmail.com>
2025-06-28 13:38:51 +0530

    Create sll.cpp
    This program demonstrates how to implement a simple singly linked list in C++.

---
+++ sll.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/*
+   Concepts Covered:
+   - Node creation using classes
+   - Dynamic memory allocation with `new`
+   - Head and tail pointers to manage linked list
+   - Function to insert node at the front of the list (push_front)
+*/
+
+// Node class representing each element of the linked list
+class Node {
+public:
+    int data;       // value stored in the node
+    Node* next;     // pointer to the next node
+
+    // Constructor to initialize a new node with a value
+    Node(int val) {
+        data = val;
+        next = NULL; // initially, no next node
+    }
+};
+
+// List class managing the linked list
+class List {
+    Node* head;   // pointer to the first node
+    Node* tail;   // pointer to the last node
+
+public:
+    // Constructor initializes head and tail to NULL
+    List() {
+        head = tail = NULL;
+    }
+
+    // Function to add a new node at the front of the list
+    void push_front(int val) {
+        Node* newNode = new Node(val); // create node dynamically
+
+        // If list is empty, both head and tail point to the new node
+        if (head == NULL) {
+            head = tail = newNode;
+            return;
+        } else {
+            // Otherwise, insert at the beginning
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+};
+
+int main() {
+    // Create a linked list object
+    List ll;
+
+    // Insert elements at the front
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+
+    // Note: Currently there is no function to print or traverse the list.
+
+    return 0;
+}
